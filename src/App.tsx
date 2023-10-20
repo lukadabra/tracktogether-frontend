cec81c4ba5886c45b0e268e7af0fd6aa9ad11481
lukadabra <luca.bousquet@gmail.com>
2023-10-20 15:25:21 +0200

    adding supertoken

---
+++ src/App.tsx
@@ -0,0 +1,100 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+import SuperTokens, { SuperTokensWrapper } from "supertokens-auth-react";
+import ThirdParty from "supertokens-auth-react/recipe/thirdparty";
+import Session from "supertokens-auth-react/recipe/session";
+
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
+import { getSuperTokensRoutesForReactRouterDom } from "supertokens-auth-react/ui";
+import { ThirdPartyPreBuiltUI } from "supertokens-auth-react/recipe/thirdparty/prebuiltui";
+import * as reactRouterDom from "react-router-dom";
+
+SuperTokens.init({
+    appInfo: {
+        appName: "track-together",
+        apiDomain: "http://localhost:5173",
+        websiteDomain: "http://localhost:5173",
+        apiBasePath: "/auth",
+        websiteBasePath: "/login"
+    },
+    recipeList: [
+        ThirdParty.init({
+            signInAndUpFeature: {
+                providers: [
+                  {
+                    id: "Deezer",
+                    name: "Deezer", // Will display "Continue with X"
+
+                    // optional
+                    // you do not need to add a click handler to this as
+                    // we add it for you automatically.
+                    buttonComponent: (props: {name: string}) => <div style={{
+                        cursor: "pointer",
+                        border: "1",
+                        paddingTop: "5px",
+                        paddingBottom: "5px",
+                        borderRadius: "5px",
+                        borderStyle: "solid"
+                    }}>{"Login with " + props.name}</div>
+                  },
+                  {
+                    id: "Spotify",
+                    name: "Spotify", // Will display "Continue with X"
+
+                    // optional
+                    // you do not need to add a click handler to this as
+                    // we add it for you automatically.
+                    buttonComponent: (props: {name: string}) => <div style={{
+                        cursor: "pointer",
+                        border: "1",
+                        paddingTop: "5px",
+                        paddingBottom: "5px",
+                        borderRadius: "5px",
+                        borderStyle: "solid"
+                    }}>{"Login with " + props.name}</div>
+                  }
+                ]
+            }
+        }),
+        Session.init()
+    ]
+});
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <SuperTokensWrapper>
+        <div>
+          <a href="https://vitejs.dev" target="_blank">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <h1>Vite + React</h1>
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <BrowserRouter>
+          <Routes>
+            {getSuperTokensRoutesForReactRouterDom(reactRouterDom, [ThirdPartyPreBuiltUI])}
+          </Routes>
+        </BrowserRouter>
+      </SuperTokensWrapper>
+    </>
+  )
+}
+
+export default App
